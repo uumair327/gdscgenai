65850a1913ada434b1251d7bbc351f17ccabf8ad
Kushal Harsora <kushalharsora13@gmail.com>
2024-05-07 13:53:28 +0530

    Installed Some dependencies

---
+++ src/pages/UploadHome.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const UploadHome = () => {
+  return (
+    <>
+        <NavLink />
+        <div className=' text-center'>
+            Upload Home
+        </div>
+    </>
+  )
+}
+
+export default UploadHome;
